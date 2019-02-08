96000cc81c9fc5964786b68cbd70d04dafc61d69
Olusola Ajayi <olusola.ajayi@andela.com>
2019-02-08 02:20:42 +0100

    feat(landing page): create landing page

--- src/Components/View/Header/LandingPageHeader/LandingPageHeader.jsx
+++ src/Components/View/Header/LandingPageHeader/LandingPageHeader.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Icon } from 'semantic-ui-react';
+import Button from '../../Button/Button';
 import Modal from '../../Modal/Modal';
 
 const LandingPageHeader = () => {
   return (
     <div className="landingPage__mobile">
-      <Link to="/search" style={{ marginRight: '10px' }}>
-        <Icon link name="search" />
+      <Link to="/search">
+        <i className="search icon" />
       </Link>
-      <Modal
-        type="login"
-        triggerEl={<Button content="Login" style={{ backgroundColor: '#2fb5ee', color: '#fff' }} />}
-      />
-      <Modal triggerEl={<Button basic content="Sign Up" style={{ boxShadow: 'none' }} />} />
+      <Link to="/login">
+        <Button content="Login" className="btn--primary header__item" />
+      </Link>
+      <Link to="/signup">
+        <Button content="Register" className="btn--default" />
+      </Link>
     </div>
   );
 };
